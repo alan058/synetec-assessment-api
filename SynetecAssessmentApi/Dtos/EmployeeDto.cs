07f39c5cf6ff97c8579a57826da42d070f4ea9fb
alan058 <40666029+alan058@users.noreply.github.com>
2022-02-04 06:53:18 +0200

    Add files via upload

--- SynetecAssessmentApi/Dtos/EmployeeDto.cs
+++ SynetecAssessmentApi/Dtos/EmployeeDto.cs
@@ -1,10 +1,12 @@
 ﻿namespace SynetecAssessmentApi.Dtos
 {
     public class EmployeeDto
-    {
+    {   
+        //Added SelectedEmployee Id for rendering employee ids in the page when the API GET request made /api/BonusPool
+        public int SelectedEmployeeId { get; set; }
         public string Fullname { get; set; }
         public string JobTitle { get; set; }
-        public int Salary { get; set; }
+        public int Salary { get; set; }      
         public DepartmentDto Department { get; set; }
     }
 }
