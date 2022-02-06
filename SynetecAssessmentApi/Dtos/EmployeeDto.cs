abdf29ac5562e945322ed47232e4bbe3d819b2f9
alan058 <40666029+alan058@users.noreply.github.com>
2022-02-06 13:19:29 +0200

    Changes made selectedEmployeeId to EmployeeId
    Changes made selectedEmployeeId to EmployeeId for user experience

--- SynetecAssessmentApi/Dtos/EmployeeDto.cs
+++ SynetecAssessmentApi/Dtos/EmployeeDto.cs
@@ -2,8 +2,8 @@
 {
     public class EmployeeDto
     {   
-        //Added SelectedEmployee Id for rendering employee ids in the page when the API GET request made /api/BonusPool
-        public int SelectedEmployeeId { get; set; }
+        //Added Employee Id for rendering employee ids in the page when the API GET request made /api/BonusPool
+        public int EmployeeId { get; set; }
         public string Fullname { get; set; }
         public string JobTitle { get; set; }
         public int Salary { get; set; }      
