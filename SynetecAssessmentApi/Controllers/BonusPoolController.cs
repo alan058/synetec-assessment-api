e4c2dd9ef647b579f10f18fb641b2becf656245f
alan058 <40666029+alan058@users.noreply.github.com>
2022-02-06 13:17:39 +0200

    Not Breaking code or not returning bad request
    if the selectedEmployeeId is not matching with the employee id from the employee List, it returns a message to the user

--- SynetecAssessmentApi/Controllers/BonusPoolController.cs
+++ SynetecAssessmentApi/Controllers/BonusPoolController.cs
@@ -2,6 +2,7 @@
 using SynetecAssessmentApi.Dtos;
 using SynetecAssessmentApi.Services;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SynetecAssessmentApi.Controllers
 {
@@ -20,10 +21,28 @@ namespace SynetecAssessmentApi.Controllers
         public async Task<IActionResult> CalculateBonus([FromBody] CalculateBonusDto request)
         {
             var bonusPoolService = new BonusPoolService();
-
-            return Ok(await bonusPoolService.CalculateAsync(
-                request.TotalBonusPoolAmount,
-                request.SelectedEmployeeId));
+            //Get all Employee List
+            var EmpList = bonusPoolService.GetEmployeesAsync();
+            //Converting to all Employee list available to a List
+            var checkEmp = EmpList.Result.ToList();
+            //counting the employee Available in the list
+            var CountEmp = checkEmp.Count();
+            //checking if the selected Employee id in the list
+            var VerifyEmpId = checkEmp.Where(x => x.EmployeeId == request.SelectedEmployeeId).FirstOrDefault();
+            
+            //returning the values based on the VerifyEmpId
+            if (VerifyEmpId!=null)
+            { //if verify id is true Return details of the employee
+                return Ok(await bonusPoolService.CalculateAsync(
+                  request.TotalBonusPoolAmount,
+                  request.SelectedEmployeeId));
+            }
+            else
+            {
+                //if verify id is not valid means selected is not matching with Employee list, then return message.               
+                return Ok("Employee Id Not Found! Please try Employee Id Between 1 to " +CountEmp + "."+ "or please do get request to see the availble Employee List.") ;
+            }
+           
         }
     }
 }
